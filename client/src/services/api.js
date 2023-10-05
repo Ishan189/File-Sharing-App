4bf8ec0030f5da9da0959804fa3824dd33458c4d
ishan1234 <ishaniv189@gmail.com>
2023-10-06 00:34:31 +0530

    trying 002

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let API_URL = 'https://file-sharing-app-w5if.onrender.com' || 'http://localhost:8000'   ;
+let API_URL = 'https://file-sharing-app-w5if.onrender.com' ;
 
 
 export const uploadFile = async (data) => {
