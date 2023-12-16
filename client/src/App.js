fc8568cd2c14f6881b70f30a0f07e7911a2dac14
Ishan189 <125630928+Ishan189@users.noreply.github.com>
2023-12-16 14:46:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,72 @@
-import {useRef, useState, useEffect} from 'react'; 
+// App.js
+import { useRef, useState, useEffect } from 'react';
 import './App.css';
-import {uploadFile} from './services/api';
+import { uploadFile } from './services/api';
 
 function App() {
   const [file, setFile] = useState('');
   const [result, setResult] = useState('');
+  const [downloadMessage, setDownloadMessage] = useState('');
 
   const fileInputRef = useRef();
-  
-const logo = 'https://i.pinimg.com/originals/8c/20/23/8c20239f1ec5777e42ae627590419576.jpg';
 
-  useEffect(()=>{
+  const logo = 'https://i.pinimg.com/originals/8c/20/23/8c20239f1ec5777e42ae627590419576.jpg';
+
+  useEffect(() => {
     const getImage = async () => {
-      if(file){
+      if (file) {
         const data = new FormData();
-        data.append("name", file.name);
-        data.append("file", file);
+        data.append('name', file.name);
+        data.append('file', file);
 
-        let response = await uploadFile(data);
-        setResult(response.path);
+        try {
+          let response = await uploadFile(data);
+          setResult(response.path);
+          setDownloadMessage('');
+        } catch (error) {
+          console.error(error);
+          setDownloadMessage('Error uploading file. Please try again.');
+        }
       }
-    }
+    };
+
     getImage();
-  }, [file])
+  }, [file]);
 
   const onUploadClick = () => {
     fileInputRef.current.click();
-  }
+  };
 
+  const onDownloadClick = () => {
+    // You can trigger the download process here
+    setDownloadMessage('Download in progress...');
+  };
 
   return (
     <div className='container'>
-      <img src={logo} alt='banner'/>
+      <img src={logo} alt='banner' />
       <div className='wrapper'>
         <h1>Simple File Sharing App!</h1>
         <p>Upload and share the download link.</p>
 
         <button onClick={() => onUploadClick()}>Upload</button>
-        <input 
-          type="file"
+        <input
+          type='file'
           ref={fileInputRef}
-          style={{display: 'none'}}
+          style={{ display: 'none' }}
           onChange={(e) => setFile(e.target.files[0])}
         />
 
-        <a href={result} target="_blank">{result}</a>
+        {result && (
+          <div>
+            <a href={result} target='_blank'>
+              {result}
+            </a>
+            <button onClick={onDownloadClick}>Download</button>
+          </div>
+        )}
+
+        {downloadMessage && <div className='download-message'>{downloadMessage}</div>}
       </div>
     </div>
   );
